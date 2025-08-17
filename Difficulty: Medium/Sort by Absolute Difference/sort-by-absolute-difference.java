7ee6725ad9a3853a17dd88272ca91d4eebc2315a
RajasriVeeramusti30 <23951A67A9@iare.ac.in>
2025-08-17 14:46:34 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Sort by Absolute Difference/sort-by-absolute-difference.java
@@ -0,0 +1,24 @@
+class Solution {
+    public void rearrange(int[] arr, int x) {
+        // code here
+        TreeMap<Integer,List<Integer>> tm=new TreeMap<>();
+        for(int i=0;i<arr.length;i++){
+            int diff=Math.abs(arr[i]-x);
+            if(!tm.containsKey(diff)){
+                List<Integer> li=new ArrayList<>();
+                li.add(arr[i]);
+                tm.put(diff,li);
+            }
+            else{
+                tm.get(diff).add(arr[i]);
+            }
+        }
+        List<Integer> ans=new ArrayList<>();
+        for(Map.Entry<Integer,List<Integer>> entry:tm.entrySet()){
+            ans.addAll(entry.getValue());
+        }
+        for(int i=0;i<arr.length;i++){
+            arr[i]=ans.get(i);
+        }
+    }
+}
